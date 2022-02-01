130231acf9f7a271964cc81957be5458735d857e
Tenzin <tenthinten@gmail.com>
2022-02-01 19:17:54 +0530

    create container

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import styled from "styled-components";
 
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 380px;
+padding: 20px 10px;
+margin: auto;
+border-radius: 4px;
+box-shadow: 0 3px 6px 0 #555;
+background: white;
+font-family: Montserrat;
 `;
 
 const CityComponent = styled.div`
