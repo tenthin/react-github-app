2b294224dd16f5f0e6d831bcdff5d42986114512
Tenzin <tenthinten@gmail.com>
2022-02-01 22:06:41 +0530

    input form to write city and button to search

--- src/modules/CityComponent.js
+++ src/modules/CityComponent.js
@@ -1,15 +1,39 @@
 import styled from "styled-components";
 
 
-const WeatherLogo = styled.image`
+const WeatherLogo = styled.img`
 width: 140px;
 height: 140px;
 margin: 40px auto;
 `
+
+const ChooseCityLabel = styled.span`
+color:black;
+font-size: 18px;
+font-weight: bold;
+margin: 10px auto;
+`
+
+const SearchBox = styled.form`
+display: flex;
+flex-direction: row;
+border: black solid 1px;
+border-radius: 2px;
+color:black;
+font-size: 18px;
+font-weight: bold;
+margin: 10px auto;
+`
+
 const CityComponent = () => {
     return (
         <>
         <WeatherLogo src = "/icons/cloudy.svg"/>
+        <ChooseCityLabel>Find Weather of your city</ChooseCityLabel>
+        <SearchBox>
+            <input placeholder="City"/>
+            <button>Search</button>
+        </SearchBox>
         </>
     )
 }
