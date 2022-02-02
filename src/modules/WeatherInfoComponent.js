5977d2d01b2c7e91b2ba4d4cf89b7a4159a93892
Tenzin <tenthinten@gmail.com>
2022-02-02 17:37:26 +0530

    changes in appearence of weather info

--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -3,23 +3,30 @@ import styled from "styled-components";
 const WeatherCondition = styled.div`
 display: flex;
 flex-direction: row;
+align-items: center;
+width: 100%;
+justitfy-content: space-between;
+margin: 30px auto;
 `
 
 const Condition = styled.span`
-display: flex;
-flex-direction: row;
+margin:20px auto;
+font-size: 14px;
+& span{
+    font-size: 28px;
+}
 `
 
 const WeatherLogo = styled.img`
-width: 140px;
-height: 140px;
-margin: 40px auto;
+width: 100px;
+height: 100px;
+margin: 5px auto;
 `
 const WeatherComponent = () => {
     return (
     <>
     <WeatherCondition>
-        <Condition>30 C</Condition>
+        <Condition><span>30 c</span> | Cloudy</Condition>
         <WeatherLogo src="/icons/cloudy.svg" />
     </WeatherCondition>
     </>
